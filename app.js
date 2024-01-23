416dca19f4f0b1c3534975a5b3bc413a7fa0c3b0
kiki <zoeluoluo05673@gmail.com>
2024-01-23 23:11:15 +0800

    feat: 抽出 utils

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
-const NodeFetchCache = require('node-fetch-cache');
+const {
+  calculateNtdByYen,
+  calculateYenByNtd,
+} = require('./utils/exchangeDollars');
 
 const app = express();
 app.use(express.json());
@@ -8,35 +11,6 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-
-const cacheFetch = NodeFetchCache.create({
-  // Only cache responses with a 2xx status code
-  shouldCacheResponse: (response) => response.ok,
-});
-
-const fetchExchangeRate = async () => {
-  const response = await cacheFetch('https://tw.rter.info/capi.php');
-  const data = await response.json();
-  return data;
-};
-
-const calculateNtdByYen = async (yen) => {
-  const result = await fetchExchangeRate();
-  // JPY => USD
-  const usd = yen / result.USDJPY.Exrate;
-  // USD => TWD
-  const twd = usd * result.USDTWD.Exrate;
-  return twd;
-};
-
-const calculateYenByNtd = async (ntd) => {
-  const result = await fetchExchangeRate();
-  // NTD => USD
-  const usd = ntd / result.USDTWD.Exrate;
-  // USD => JPY
-  const yen = usd * result.USDJPY.Exrate;
-  return yen;
-};
 
 const getNtdByYen = async (req, res) => {
   const { yen } = req.body;
