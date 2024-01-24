ffade1d5c7390bf604d7f99d163b890df15c574a
kiki <zoeluoluo05673@gmail.com>
2024-01-24 12:51:47 +0800

    feat: 增加 tours api mock data & CRUD

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const fs = require('fs');
+const short = require('short-uuid');
 const {
   calculateNtdByYen,
   calculateYenByNtd,
@@ -7,10 +9,150 @@ const {
 
 const app = express();
 app.use(express.json());
+const translator = short();
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// TODO: implement cors on /tour api
+
+const getTours = (req, res) => {
+  fs.readFile(`${__dirname}/data/tours.json`, 'utf-8', (err, data) => {
+    if (err) {
+      console.error(`💥 Error: ${err}`);
+    }
+    res.status(200).json({
+      status: 'success',
+      results: JSON.parse(data).length,
+      data: {
+        tours: JSON.parse(data),
+      },
+    });
+  });
+};
+
+const createTour = (req, res) => {
+  const newId = translator.generate();
+  const newTour = Object.assign({ id: newId }, req.body);
+  // req.body should look like this
+  /*
+  {
+    "timestamp": 1706066106947,
+    "name": "六花亭買伴手禮",
+    "location": "https://maps.app.goo.gl/n3Nsi8DYhr8iUXcW9",
+    "todo": []
+  }
+  */
+  fs.readFile(`${__dirname}/data/tours.json`, 'utf-8', (err, data) => {
+    if (err) {
+      console.error(`💥 Error: ${err}`);
+    }
+    const newData = JSON.parse(data);
+    newData.push(newTour);
+    fs.writeFile(
+      `${__dirname}/data/tours.json`,
+      JSON.stringify(newData),
+      (err) => {
+        if (err) {
+          console.error(`💥 Error: ${err}`);
+        }
+        res.status(201).json({
+          status: 'success',
+          data: {
+            tour: newTour,
+          },
+        });
+      },
+    );
+  });
+};
+
+const getTour = (req, res) => {
+  const { id } = req.params;
+  fs.readFile(`${__dirname}/data/tours.json`, 'utf-8', (err, data) => {
+    if (err) {
+      console.error(`💥 Error: ${err}`);
+    }
+    const parsedData = JSON.parse(data);
+    const tour = parsedData.find((el) => el.id === id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  });
+};
+
+const deleteTour = (req, res) => {
+  const { id } = req.params;
+  fs.readFile(`${__dirname}/data/tours.json`, 'utf-8', (err, data) => {
+    if (err) {
+      console.error(`💥 Error: ${err}`);
+    }
+    const parsedData = JSON.parse(data);
+    const newTour = parsedData.filter((el) => el.id !== id);
+    fs.writeFile(
+      `${__dirname}/data/tours.json`,
+      JSON.stringify(newTour),
+      (err) => {
+        if (err) {
+          console.error(`💥 Error: ${err}`);
+        }
+        res.status(201).json({
+          status: 'success',
+          data: {
+            tour: newTour,
+          },
+        });
+      },
+    );
+  });
+};
+
+const patchTour = (req, res) => {
+  const { id } = req.params;
+  fs.readFile(`${__dirname}/data/tours.json`, 'utf-8', (err, data) => {
+    if (err) {
+      console.error(`💥 Error: ${err}`);
+    }
+    const parsedData = JSON.parse(data);
+    const adjustedTour = parsedData.map((el) => {
+      if (el.id === id) {
+        // req.body should look like this
+        /*
+        {
+          "name": "六花亭買伴手禮",
+        }
+        */
+        return Object.assign(el, req.body);
+      }
+      return el;
+    });
+    fs.writeFile(
+      `${__dirname}/data/tours.json`,
+      JSON.stringify(adjustedTour),
+      (err) => {
+        if (err) {
+          console.error(`💥 Error: ${err}`);
+        }
+        res.status(201).json({
+          status: 'success',
+          data: {
+            tour: adjustedTour,
+          },
+        });
+      },
+    );
+  });
+};
+
+app.route('/api/v1/tours').get(getTours).post(createTour);
+app.route('/api/v1/tours/:id').get(getTour).delete(deleteTour).patch(patchTour);
+
+//app.route('/api/v1/tours/:id/todos').post(createTodo);
+//app.route('/api/v1/tours/:id/todos/:todoId').patch(patchTodo).delete(deleteTodo);
 
 ////// start - 串接 line message api 之後改成從 line SDK 輸出結果 //////
 const getNtdByYen = async (req, res) => {
