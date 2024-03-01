ce7db59a736081286f03011fa9b7c3973f9900d2
kiki <zoeluoluo05673@gmail.com>
2024-03-01 22:55:11 +0800

    feat: add virtual property

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,19 +4,34 @@ const todoSchema = new mongoose.Schema({
   checked: { type: Boolean, default: false },
 });
 
-const tourSchema = new mongoose.Schema({
-  userID: { type: String, required: [true, 'missing userID'] },
-  date: { type: Date, required: [true, 'missing date'] },
-  duration: { type: Number, required: [true, 'missing time'] },
-  priority: {
-    type: Number,
-    required: [true, 'missing priority'],
-    min: 1,
-    max: 5,
+const tourSchema = new mongoose.Schema(
+  {
+    userID: { type: String, required: [true, 'missing userID'] },
+    date: { type: Date, required: [true, 'missing date'] },
+    duration: { type: Number, required: [true, 'missing time'] },
+    priority: {
+      type: Number,
+      required: [true, 'missing priority'],
+      min: 1,
+      max: 5,
+    },
+    name: { type: String, required: [true, 'missing name'] },
+    location: { type: String },
+    todo: { type: [todoSchema], default: [] },
   },
-  name: { type: String, required: [true, 'missing name'] },
-  location: { type: String },
-  todo: { type: [todoSchema], default: [] },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
+
+tourSchema.virtual('formattedDate').get(function () {
+  const formattedDate = new Date(this.date).toLocaleDateString('zh-tw', {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+  });
+  return formattedDate;
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
